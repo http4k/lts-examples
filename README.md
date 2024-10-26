fd77cfd085873b1a295ae1020b0574822ae80d4d
David Denton <denton.david@gmail.com>
2024-10-26 13:44:17 +0100

    Adding code for example

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# lts-examples
+# http4k LTS examples
 Examples of how to configure the http4k LTS edition
---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.gradle.api.JavaVersion.VERSION_11
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+            jvmTarget.set(JVM_11)
+            freeCompilerArgs.add("-Xjvm-default=all")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        sourceCompatibility = VERSION_11
+        targetCompatibility = VERSION_11
+    }
+}
+
+dependencies {
+    val http4kVersion = "5.32.4.0"
+
+    implementation(platform("org.http4k:http4k-bom:$http4kVersion"))
+    implementation("org.http4k:http4k-core")
+
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.1")
+}
